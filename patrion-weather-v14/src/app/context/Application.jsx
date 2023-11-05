fc6ee6c9fc780493def2278be364e006e6bbc1ed
Volkan Uğur ARSLAN <volkanugurarslan@gmail.com>
2023-11-06 01:52:30 +0300

    [F-4] Map Application + Responsive
    Added Map Option.
    Optimized Functionality.

--- patrion-weather-v14/src/app/context/Application.jsx
+++ patrion-weather-v14/src/app/context/Application.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import localization from "../../../public/languages/en-en.json"
 import Image from "next/image";
 import Result from "@/components/Result";
@@ -13,6 +13,7 @@ export default function Application() {
 
     const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=metric`;
 
+    // Function to fetch weather data and update state
     const fetchWeather = (e) => {
         e.preventDefault();
         setLoading(true);
@@ -30,18 +31,20 @@ export default function Application() {
         ;
     };
 
+    // Function to track user input for city search
     const trackSearch = (e) => {
         e.preventDefault();
         setCity(e.target.value)
     }
 
+    // Function to clear weather data
     const clearSearch = (e) => {
         e.preventDefault();
         setWeather("");
     }
 
     return (
-        <div className="relative isolate overflow-hidden bg-gray-900 px-12 py-24 sm:py-32" id="application">
+        <div className="relative isolate overflow-hidden bg-gray-900 px-12 py-24 sm:py-32 lg:hidden" id="application-mobile">
             <div className="absolute w-full h-full top-0 left-0 bg-sky-500/25">
                 <Image
                     src="https://images.unsplash.com/photo-1416163347366-de4602bbb003?auto=format&fit=crop&q=80&w=1471&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -55,7 +58,7 @@ export default function Application() {
                 <div className="mx-auto lg:mx-0">
                     <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">{localization.application.title}</h2>
                     <p className="mt-6 text-lg leading-8 text-gray-300">
-                        {localization.application.description}
+                        {localization.application.description_search}
                     </p>
                 </div>
 
@@ -78,14 +81,14 @@ export default function Application() {
 
                         <button
                             onClick={fetchWeather}
-                            className="mt-3 inline-flex w-full items-center justify-center rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600 sm:ml-3 sm:mt-0 sm:w-auto"
+                            className="mt-3 inline-flex w-full items-center justify-center rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600 sm:ml-3 sm:mt-0 sm:w-auto max-w-[10rem]"
                         >
                             {localization.search.action}
                         </button>
 
                         <button
                             onClick={clearSearch}
-                            className="mt-3 inline-flex w-full items-center justify-center rounded-md bg-sky-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-700 sm:ml-3 sm:mt-0 sm:w-auto"
+                            className="mt-3 inline-flex w-full items-center justify-center rounded-md bg-sky-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-700 sm:ml-3 sm:mt-0 sm:w-auto ml-2 max-w-[2rem]"
                         >
                             {localization.search.clear}
                         </button>
--- patrion-weather-v14/src/app/page.js
+++ patrion-weather-v14/src/app/page.js
@@ -7,6 +7,7 @@ import OpenWeather from "./context/OpenWeather";
 import Application from "./context/Application";
 import Tailwind from "./context/Tailwind";
 import NextJS from "./context/NextJS";
+import TurkeyMap from "./context/TurkeyMap";
 
 export default function Home() {
     return (
@@ -15,6 +16,7 @@ export default function Home() {
 
             <main className="min-h-screen">
                 <Application />
+                <TurkeyMap />
                 <OpenWeather />
                 <NextJS />
                 <Tailwind />
